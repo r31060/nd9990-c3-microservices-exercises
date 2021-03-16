8d21fd99ab3de782df8536ea1a8525b970e75b14
r31060 <jgillies@gmail.com>
2021-03-16 10:57:43 -0700

    deployment commit

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=udagramjgdev
+export POSTGRES_PASSWORD=Welcome1!
+export POSTGRES_HOST=udagramjgdev.c8pcurrsdryw.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-jbgillie-dev2
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
 export URL=http://localhost:8100
